d760f1056ad081ecf51e5e8e72b415d041998868
Aliaksandr <byshlata@yandex.ru>
2022-12-07 22:31:41 +0300

    Add policy

--- src/server/index.ts
+++ src/server/index.ts
@@ -35,7 +35,7 @@ app.use(cors(corsOptions))
 app.use(express.json())
 app.use(cookieParser());
 
-app.user('/privacy-policy', express.static('public'))
+app.use('/privacy-policy', express.static('public'))
 
 app.use(`${Path.Register}`, register)
 app.use(`${Path.Login}`, login)
